b799a004e188e9bf8febb151c13e6e87529829f4
Mudit Sahni <muditsahni@msn.com>
2024-03-24 16:46:11 +0530

    out dir

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'export',
+
+    // Adjust basePath and assetPrefix for GitHub Pages if necessary
+    basePath: '/site',
+    assetPrefix: '/site/',
+};
 
 export default nextConfig;
